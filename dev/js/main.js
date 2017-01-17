f37214eb0b10b6cb5ba644fa8c06f9c0910aeecc
Quentin <quentin.lemarchand@hetic.net>
2017-01-17 17:33:24 +0100

    add score

--- dev/js/main.js
+++ dev/js/main.js
@@ -2,10 +2,14 @@
 var random_color, // random color storage for building combination
 		selected_color, // clicked color by user
 		cases_index    = 0, // try index
+		good_pos_nb    = 0,
+		good_color_nb  = 0,
 		combination    = [], // storage for final solution
 		colors         = ['red', 'green', 'blue', 'orange', 'yellow', 'violet', 'pink', 'cyan'] // possible colors
 		cases          = document.querySelectorAll('.case'), // storage for cases
 		color_picker   = document.querySelectorAll('.color'), // storage for colors
+		good_pos       = document.querySelector('.good-pos>span'), // well positioned cases in proposition
+		good_color     = document.querySelector('.good-color>span'), // found colors cases in proposition
 		validation_btn = document.querySelector('.validation'); // verify btn
 
 
@@ -83,17 +87,27 @@ function verifyCombination()
 {
 	validation_btn.addEventListener('click', function()
 	{
+		good_pos_nb   = 0;
+		good_color_nb = 0;
+
+		good_pos.innerHTML   = good_pos_nb;
+		good_color.innerHTML = good_color_nb;
+
 		for(var i = cases_index; i < cases_index + 4; i++)
 		{
 			for(var j = 0; j < 4; j++)
 			{
 				// same color
-				if(cases[i].style.backgroundColor == combination[j])
+				if(cases[i].style.backgroundColor == combination[j] && i != j + cases_index)
 				{
-					// same color and same pos
-					if(i == j + cases_index)
-					{
-					}
+					good_color_nb += 1;
+					good_color.innerHTML = good_color_nb;
+				}
+				// same color and same pos
+				 else if(cases[i].style.backgroundColor == combination[j] && i == j + cases_index)
+				{
+					good_pos_nb += 1;
+					good_pos.innerHTML = good_pos_nb;
 				}
 			}
 		}
